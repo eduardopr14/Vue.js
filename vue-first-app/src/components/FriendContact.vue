79c75d26e8380d79002c3755afec5b60b1d76cc0
eduardo.ruli@solinftec.com <eduardo.ruli@solinftec.com>
2021-01-29 16:30:55 -0300

    vue 02

---
+++ vue-first-app/src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails">Show Details</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone: </strong>{{ friend.phone }}</li>
+            <li><strong>Email: </strong>{{ friend.email }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                detailsAreVisible: false,
+                friend: {
+                            id: 'Manuel',
+                            name: 'Manuel Otto',
+                            phone: '0123456789',
+                            email: 'manuel@email.com'
+                        }
+            };
+        },
+        methods: {
+            toggleDetails() {
+                this.detailsAreVisible = !this.detailsAreVisible;
+            }
+        }
+    };
+</script>
