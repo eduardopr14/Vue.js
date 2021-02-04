ad67f9dce705053b93047bbae9c1bc78ec628fa9
eduardo.ruli@solinftec.com <eduardo.ruli@solinftec.com>
2021-02-04 15:12:15 -0300

    practice project

---
+++ cmp-communication-assignment-problem/src/components/ActiveUser.vue
@@ -0,0 +1,18 @@
+<template>
+    <li>
+        <h2>{{ username }}</h2>
+        <h3>{{ age }}</h3>
+        <button @click="$emit('delete', id)">Delete</button>
+    </li>
+</template>
+
+<script>
+    export default {
+        props: {
+            id: {type: String, required: true},
+            username: {type: String, required: true},
+            age: {type: String, required: true}
+        },
+        emits: ['delete']
+    };
+</script>
