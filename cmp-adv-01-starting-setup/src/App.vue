cb3b89644b63332987ebf84ec9223f254f9a5be9
eduardo.ruli@solinftec.com <eduardo.ruli@solinftec.com>
2021-02-11 14:31:26 -0300

    local import component

--- cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -11,7 +11,13 @@
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+
 export default {
+  components: {
+    'the-header': TheHeader
+    // Pode ser escrito como TheHeader: TheHeader e ser utilizado com a tag <TheHeader />
+  },
   data() {
     return {
       activeUser: {
