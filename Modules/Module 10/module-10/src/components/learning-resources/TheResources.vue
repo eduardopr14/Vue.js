35d05010b81685512ecc1c134255d0ef06548f54
eduardo.ruli@solinftec.com <eduardo.ruli@solinftec.com>
2021-02-14 11:38:48 -0300

    dynamic components

---
+++ Modules/Module 10/module-10/src/components/learning-resources/TheResources.vue
@@ -0,0 +1,33 @@
+<template>
+  <base-card>
+    <base-button @click="setSelectedTab('stored-resources')">Stored Resources</base-button>
+    <base-button @click="setSelectedTab('add-resources')">Add Resource</base-button>
+  </base-card>
+  <component :is="selectedTab"></component>
+</template>
+
+<script>
+import StoredResources from './StoredResource.vue';
+import AddResources from './AddResources.vue';
+
+export default {
+    components: {
+        StoredResources,
+        AddResources
+    },
+    data() {
+        return {
+            selectedTab: 'stored-resources'
+        };
+    },
+    methods: {
+        setSelectedTab(tab) {
+            this.selectedTab = tab;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
