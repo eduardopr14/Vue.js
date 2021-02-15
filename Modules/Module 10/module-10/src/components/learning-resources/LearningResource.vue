b65e6323b89cf19abc8049e2ce9cb0807bebd85e
eduardo.ruli@solinftec.com <eduardo.ruli@solinftec.com>
2021-02-15 09:39:09 -0300

    delete

--- Modules/Module 10/module-10/src/components/learning-resources/LearningResource.vue
+++ Modules/Module 10/module-10/src/components/learning-resources/LearningResource.vue
@@ -3,7 +3,7 @@
     <base-card>
       <header>
         <h3>{{ title }}</h3>
-        <base-button mode="flat">Delete</base-button>
+        <base-button mode="flat" @click="deleteResource(id)">Delete</base-button>
       </header>
       <p>{{ description }}</p>
       <nav>
@@ -17,7 +17,8 @@
 import BaseCard from '../UI/BaseCard.vue'
 export default {
   components: { BaseCard },
-    props: ['title', 'description', 'link']
+    props: ['id', 'title', 'description', 'link'],
+    inject: ['deleteResource']
 }
 </script>
 
