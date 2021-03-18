1a9807c7367b1ce6f314a7be89e3f38e346d0bdc
eduardo.ruli@solinftec.com <eduardo.ruli@solinftec.com>
2021-03-18 11:01:39 -0300

    route metadata

--- Modules/Module 13/module-13/src/main.js
+++ Modules/Module 13/module-13/src/main.js
@@ -16,6 +16,7 @@ const router = createRouter({
         { 
             name: 'teams',
             path: '/teams', 
+            meta: { needsAuth: true },
             components: { default: TeamsList, footer: TeamsFooter }, 
             children: [
                 { name: 'team-members', path: ':teamId', component: TeamMembers, props: true }
@@ -48,6 +49,12 @@ const router = createRouter({
 router.beforeEach(function(to, from, next) {
     console.log('Global beforeEach');
     console.log(to, from);
+    if (to.meta.needsAuth) {
+        console.log('Needs auth!');
+        next();
+    } else {
+        next();
+    }
     // if (to.name === 'team-members') {
     //     next();
     // } else {
