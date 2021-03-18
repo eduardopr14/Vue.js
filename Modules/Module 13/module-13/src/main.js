fadfa1d853a787b6fa039fc381f2656d3131dab9
eduardo.ruli@solinftec.com <eduardo.ruli@solinftec.com>
2021-03-18 10:57:39 -0300

    route leave guards

--- Modules/Module 13/module-13/src/main.js
+++ Modules/Module 13/module-13/src/main.js
@@ -46,14 +46,20 @@ const router = createRouter({
 });
 
 router.beforeEach(function(to, from, next) {
-    // console.log('Global beforeEach');
-    // console.log(to, from);
+    console.log('Global beforeEach');
+    console.log(to, from);
     // if (to.name === 'team-members') {
     //     next();
     // } else {
     //     next({ name: 'team-members', params: { teamId: 't2' }});
     // }
     next();
+});
+
+router.afterEach(function(to, from) {
+    // sending analytics data
+    console.log('Global afterEach');
+    console.log(to, from);
 });
 
 const app = createApp(App)
